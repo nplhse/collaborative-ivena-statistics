36b63c58664a0ec21efcbfc0868787dc1f2f6316
Christoph Steltner <mail@nplhse.com>
2025-07-07 18:24:32 +0200

    build(style): Cleared some minor issues

--- rector.php
+++ rector.php
@@ -6,11 +6,11 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/assets',
-        __DIR__ . '/config',
-        __DIR__ . '/public',
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
+        __DIR__.'/assets',
+        __DIR__.'/config',
+        __DIR__.'/public',
+        __DIR__.'/src',
+        __DIR__.'/tests',
     ])
     // uncomment to reach your current PHP version
     // ->withPhpSets()
