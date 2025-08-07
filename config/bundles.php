de0ca181d40973af7bcded1208e2f9c10e32e9c4
Christoph Steltner <mail@nplhse.com>
2025-08-07 13:53:44 +0200

    feat: Added dama/doctrine-test-bundle for faster database resets

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
