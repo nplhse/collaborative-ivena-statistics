dbc5a273cf8d5472ed3434f72bfd36552bfe58ac
Christoph Steltner <mail@nplhse.com>
2025-08-08 17:23:33 +0200

    feat: Added Controller to display Areas as datatable

--- src/Controller/Data/Area/ListAreaController.php
+++ src/Controller/Data/Area/ListAreaController.php
@@ -2,15 +2,28 @@
 
 namespace App\Controller\Data\Area;
 
+use App\DataTransferObjects\AreaListQueryParametersDTO;
+use App\Repository\DispatchAreaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryString;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ListAreaController extends AbstractController
 {
+    public function __construct(
+        private DispatchAreaRepository $dispatchAreaRepository,
+    ) {
+    }
+
     #[Route('/data/area', name: 'app_data_area_list')]
-    public function index(): Response
-    {
-        return $this->render('data/area/list.html.twig');
+    public function index(
+        #[MapQueryString] AreaListQueryParametersDTO $query,
+    ): Response {
+        $results = $this->dispatchAreaRepository->findAreasByQueryParameterDTO($query);
+
+        return $this->render('data/area/list.html.twig', [
+            'results' => $results,
+        ]);
     }
 }
---
+++ src/DataTransferObjects/AreaListQueryParametersDTO.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DataTransferObjects;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+final readonly class AreaListQueryParametersDTO
+{
+    public function __construct(
+        #[Assert\GreaterThan(0)]
+        public int $page = 1,
+
+        #[Assert\Range(min: 1, max: 100)]
+        public int $limit = 25,
+
+        #[Assert\Choice(choices: ['asc', 'desc'])]
+        public string $orderBy = 'desc',
+    ) {
+    }
+}
