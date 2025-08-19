b56560d130fc8d571b6fba460e4e8368bbc8354b
Christoph Steltner <mail@nplhse.com>
2025-08-19 12:40:50 +0200

    feat: Added basic Import controllers

--- src/Controller/Import/ImportController.php
+++ src/Controller/Import/ImportController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller\Import;
 
+use App\DataTransferObjects\ListImportQueryParametersDTO;
+use App\Repository\ImportRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryString;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/import', name: 'app_import_index')]
 final class ImportController extends AbstractController
 {
-    public function __invoke(): Response
-    {
+    public function __construct(
+        private readonly ImportRepository $importRepository,
+    ) {
+    }
+
+    public function __invoke(
+        #[MapQueryString] ListImportQueryParametersDTO $query,
+    ): Response {
+        $paginator = $this->importRepository->getPaginator($query);
+
         return $this->render('import/index.html.twig', [
-            'controller_name' => 'ImportController',
+            'paginator' => $paginator,
+            'pagination_route' => 'app_import_index',
+            'search' => $query->search,
+            'sortBy' => $query->sortBy,
+            'orderBy' => $query->orderBy,
         ]);
     }
 }
