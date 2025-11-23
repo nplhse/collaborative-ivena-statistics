d55bba9cd3174c8a9d261edc9770e4383342aa5e
Christoph Steltner <mail@nplhse.com>
2025-11-24 00:04:27 +0100

    deps(dev): Disabled dama-doctrine-test bundle due to strange errors...

--- config/bundles.php
+++ config/bundles.php
@@ -16,6 +16,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
-    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
 ];
