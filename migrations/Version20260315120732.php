edb23f1085b6d42fb865b37e2cf3e8de9d2b3c0f
Christoph Steltner <mail@nplhse.com>
2026-03-15 14:20:45 +0100

    fix: Corrected faulty name of the migration

--- migrations/Version20260315120732.php
+++ migrations/Version20260315120732.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20250315120000 extends AbstractMigration
+final class Version20260315120732 extends AbstractMigration
 {
     public function getDescription(): string
     {
