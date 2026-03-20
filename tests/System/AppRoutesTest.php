d1f8f92cc4a0478912b7132708cbc99778fe87a7
Christoph Steltner <mail@nplhse.com>
2026-03-20 23:17:52 +0100

    test: Added new routes to tests.

--- tests/System/AppRoutesTest.php
+++ tests/System/AppRoutesTest.php
@@ -44,10 +44,16 @@ class AppRoutesTest extends WebTestCase
     {
         yield 'app_default' => ['/'];
         yield 'app_login' => ['/login'];
+        yield 'app_register' => ['/register'];
+        yield 'app_forgot_password_request' => ['/reset-password'];
+        yield 'app_check_email' => ['/reset-password/check-email'];
     }
 
     public static function getSecureUrls(): ?\Generator
     {
         yield 'app_admin_dashboard' => ['/admin/'];
+        yield 'app_settings_index' => ['/settings'];
+        yield 'app_settings_email' => ['/settings/email'];
+        yield 'app_settings_password' => ['/settings/password'];
     }
 }
