d1f7aaac4231d04d6151e6337cdedaedfc096073
Christoph Steltner <mail@nplhse.com>
2026-03-23 22:04:53 +0100

    build: Added code quality rules to rector.

--- rector.php
+++ rector.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 
+$entityPath = __DIR__.'/src/**/Domain/Entity/*';
+
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/src',
@@ -12,6 +14,10 @@ return RectorConfig::configure()
     ->withPhpSets(php84: true)
     ->withPreparedSets(
         deadCode: true,
+        codeQuality: true,
     )
     ->withTypeCoverageLevel(0)
-    ->withCodeQualityLevel(0);
+    ->withSkip([
+        Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector::class => [$entityPath],
+        Rector\Php81\Rector\Property\ReadOnlyPropertyRector::class => [$entityPath],
+    ]);
