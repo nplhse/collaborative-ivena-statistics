976523ec8dff0ef73f3a6fb9f348f6654aa909af
Christoph Steltner <mail@nplhse.com>
2026-03-23 20:06:08 +0100

    build: Switched to rector rules matching PHP 8.4

--- rector.php
+++ rector.php
@@ -6,14 +6,10 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__.'/assets',
-        __DIR__.'/config',
-        __DIR__.'/public',
         __DIR__.'/src',
         __DIR__.'/tests',
     ])
-    // uncomment to reach your current PHP version
-    // ->withPhpSets()
+    ->withPhpSets(php84: true)
     ->withTypeCoverageLevel(0)
     ->withDeadCodeLevel(0)
     ->withCodeQualityLevel(0);
