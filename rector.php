1ad5be3c508fa33aa8d777d70e93923ec40a873d
Christoph Steltner <mail@nplhse.com>
2026-03-23 22:51:59 +0100

    build: Some final tweaks with adding cache& parallel execution to rector.

--- rector.php
+++ rector.php
@@ -8,22 +8,27 @@ $entityPath = __DIR__.'/src/**/Domain/Entity/*';
 
 return RectorConfig::configure()
     ->withPaths([
+        __DIR__.'/config',
         __DIR__.'/src',
         __DIR__.'/tests',
     ])
-    ->withPhpSets(php84: true)
+    ->withPhpSets(
+        php84: true
+    )
     ->withPreparedSets(
         deadCode: true,
         codeQuality: true,
         typeDeclarations: true,
     )
     ->withSets([
+        Rector\Symfony\Set\SymfonySetList::CONFIGS,
         Rector\Symfony\Set\SymfonySetList::SYMFONY_CODE_QUALITY,
-    ]
-    )
+        Rector\Symfony\Set\SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+    ])
     ->withSkip([
         Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector::class => [$entityPath],
         Rector\Php81\Rector\Property\ReadOnlyPropertyRector::class => [$entityPath],
         Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector::class => [$entityPath],
-        Rector\TypeDeclaration\Rector\ClassMethod\AddParamTypeDeclarationRector::class => [$entityPath],
-    ]);
+    ])
+    ->withCache(__DIR__.'/var/cache/rector')
+    ->withParallel();
