0faa7b2fbb5d7c4e78dd914901106c8c9c0e7124
Christoph Steltner <mail@nplhse.com>
2026-07-29 22:40:15 +0200

    chore(kernel): bump app version to v0.0.5-alpha

--- src/Kernel.php
+++ src/Kernel.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 final class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-    public const string APP_VERSION = 'v0.0.4-alpha';
+    public const string APP_VERSION = 'v0.0.5-alpha';
 
     #[\Override]
     public function boot(): void
